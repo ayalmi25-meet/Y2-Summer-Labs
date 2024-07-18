9df7601edbd4f4687c331f1fbe3327326763fa1e
ayalmi25-meet <ayalmiller@gmail.com>
2024-07-18 11:33:02 +0300

    channge works

--- 0.6. Advanced Flask fortune-teller/app.py
+++ 0.6. Advanced Flask fortune-teller/app.py
@@ -6,18 +6,18 @@ app = Flask(__name__, template_folder = "templates")
 @app.route('/home', methods=["GET", "POST"])
 def home():
 	if request.method == 'GET':
-		return render_template("hello.html")
+		return render_template("home.html")
 	else:
 		birth_month = request.form['birthmonth']
 		return redirect(url_for('fortune', birth_month=birth_month))	
 
-@app.route('/fortune/<birth_month>')
+@app.route('/fortune/<string:birth_month>')
 def fortune(birth_month):
 	fortunes = ["free pass microfeedback:)","to be meet ta", "to have 1000000 meet coins", "to win 10000000 shekels","to eat pasta in iasa", "to have curfew every day at 12 pm", "to have permission talking yout mother langauge", "to become sari","to meet with messi", "to have free time all day"]
 	index=len(birth_month)
 	chosen_one=fortunes[9]
 	if index<10:
-		chosen=fortunes[index-1]
+		chosen_one=fortunes[index]
 	return render_template("fortune.html" , chosen=chosen_one)
 
 	
